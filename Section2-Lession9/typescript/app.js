88ab21db2e78730bacd096c716162c090273fcd4
Fernando Luján Martínez <8flm95@gmail.com>
2020-11-30 19:57:41 +0100

    Added initial typescript code

---
+++ Section2-Lession9/typescript/app.js
@@ -0,0 +1,8 @@
+"use strict";
+(function () {
+    var mensaje = 'hola';
+    if (true) {
+        var mensaje_1 = 'holii';
+    }
+    console.log(mensaje);
+})();
---
+++ Section2-Lession9/typescript/app.ts
@@ -0,0 +1,11 @@
+(function() {
+
+  let mensaje = 'hola'
+
+  if(true) {
+
+    let mensaje = 'holii'
+  }
+  console.log(mensaje)
+
+})();
